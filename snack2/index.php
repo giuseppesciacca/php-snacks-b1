75a9cd46eeae2a24468d62cfc78401b228579a87
giuseppesciacca <sciaccagi@gmail.com>
2023-05-04 12:49:02 +0200

    [add] btn in form + if name length >3

--- snack2/index.php
+++ snack2/index.php
@@ -1,6 +1,19 @@
 <!-- ## Snack 2
 
-Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, 
+che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+$name = $_GET["name"];
+
+if (strlen($name) > 3) {
+    var_dump('maggiore');
+}
+
+var_dump($name);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +22,7 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack2</title>
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
@@ -20,13 +33,16 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
         <form action="" method="get">
             <div class="mb-3">
                 <label for="" class="form-label">Name</label>
-                <input type="text" name="" id="" class="form-control" placeholder="name" aria-describedby="helpId">
+                <input type="text" name="name" id="" class="form-control" placeholder="name" aria-describedby="helpId">
 
                 <label for="" class="form-label">Mail</label>
-                <input type="mail" name="" id="" class="form-control" placeholder="email" aria-describedby="helpId">
+                <input type="mail" name="mail" id="" class="form-control" placeholder="email" aria-describedby="helpId">
 
                 <label for="" class="form-label">Number</label>
-                <input type="number" placeholder="Age" class="form-control">
+                <input type="number" placeholder="Age" name="age" class="form-control">
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="reset" class="btn btn-primary">Reset</button>
             </div>
         </form>
     </div>
