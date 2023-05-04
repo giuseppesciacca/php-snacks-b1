1468b9df92d67b11e68ee76f6836cc6fb3cb3ccc
giuseppesciacca <sciaccagi@gmail.com>
2023-05-04 12:25:38 +0200

    [add] foreach

--- snack1/index.php
+++ snack1/index.php
@@ -7,3 +7,33 @@ punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema.
 
 Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+$matches = [
+    [
+        'Olimpia Milano' => 'Cantù',
+        55 => 60,
+    ],
+    [
+        'GeVi Napoli Basket' => 'Carpegna Prosciutto Pesaro',
+        98 => 87,
+    ],
+    [
+        'Germani Brescia' => 'Happy Casa Brindisi',
+        75 => 69,
+    ],
+    [
+        'Bertram Yachts Derthona Tortona' => 'EA7 Emporio Armani Milano',
+        75 => 77,
+    ],
+    [
+        'Banco di Sardegna Sassari' => 'UNAHOTELS Reggio Emilia',
+        89 => 77,
+    ],
+];
+
+foreach ($matches as $match) {
+    foreach ($match as $key => $value) {
+        echo $key . ' - ' . ' | ' . $value;
+    };
+};
