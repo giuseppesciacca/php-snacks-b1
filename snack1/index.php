7e5b9e6cc622a2f21b081ccfbd2a83747a94ef63
giuseppesciacca <sciaccagi@gmail.com>
2023-05-04 12:29:45 +0200

    [fix] foreach

--- snack1/index.php
+++ snack1/index.php
@@ -34,6 +34,34 @@ $matches = [
 
 foreach ($matches as $match) {
     foreach ($match as $key => $value) {
-        echo $key . ' - ' . ' | ' . $value;
+        echo $key . ' - ' . $value . ' | ';
     };
-};
+}; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    foreach ($matches as  $match) : ?>
+        <p>
+            <?= $key . ': ' . $value ?>
+        </p>
+    <?php endforeach ?>
+
+    <?php
+    foreach ($_GET as $key => $value) : ?>
+        <p>
+            <?= $key . ': ' . $value ?>
+        </p>
+    <?php endforeach ?>
+</body>
+
+</html>
