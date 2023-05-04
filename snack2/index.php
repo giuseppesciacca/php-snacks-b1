4e89bedfb2bd9837c725f9db28a1bd72fac00c63
giuseppesciacca <sciaccagi@gmail.com>
2023-05-04 12:42:58 +0200

    [add] form on html

--- snack2/index.php
+++ snack2/index.php
@@ -1,3 +1,40 @@
 <!-- ## Snack 2
 
 Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <form action="" method="get">
+            <div class="mb-3">
+                <label for="" class="form-label">Name</label>
+                <input type="text" name="" id="" class="form-control" placeholder="name" aria-describedby="helpId">
+
+                <label for="" class="form-label">Mail</label>
+                <input type="mail" name="" id="" class="form-control" placeholder="email" aria-describedby="helpId">
+
+                <label for="" class="form-label">Number</label>
+                <input type="number" placeholder="Age" class="form-control">
+            </div>
+        </form>
+    </div>
+
+
+
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/disjs/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
